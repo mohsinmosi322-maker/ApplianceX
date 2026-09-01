d781646575da59b1f3cf0f5aa0b95276a8cc4e0b
mohsinmosi322-maker <mohsinmosi322@gmail.com>
2026-09-01 10:01:22 +0500

    Fix csproj missing files; Release: no PDB package; SuppressIldasm anti-decompile attribute

---
+++ V6/ApplianceManagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("ApplianceX")]
+[assembly: AssemblyDescription("Appliance Management POS")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ApplianceX")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
+
+// Mild anti-decompile signal (ILDasm). Not a substitute for commercial obfuscation.
+[assembly: SuppressIldasm]
