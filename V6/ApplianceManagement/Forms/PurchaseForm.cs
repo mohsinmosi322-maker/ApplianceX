9059ad7b8659a904ee959245c78042bbe1e7d836
mohsinmosi322-maker <mohsinmosi322@gmail.com>
2026-09-02 09:46:41 +0500

    F4 exit confirm + F1 live cart rates on Purchase

--- V6/ApplianceManagement/Forms/PurchaseForm.cs
+++ V6/ApplianceManagement/Forms/PurchaseForm.cs
@@ -54,7 +54,8 @@ namespace ApplianceManagement.Forms
             MinimumSize = new Size(900, 560);
             BackColor = UiHelper.BgColor;
             KeyPreview = true;
-            UiHelper.AttachF4Close(this, false);
+            UiHelper.AttachF4Close(this, true);
+            UiHelper.AttachEnterNavigation(this);
 
             dgv = new DataGridView { Dock = DockStyle.Fill };
             UiHelper.StyleGrid(dgv);
@@ -152,6 +153,7 @@ namespace ApplianceManagement.Forms
             lstSupplier = new ListBox
             {
                 Visible = false,
+                Location = new Point(-2000, -2000),
                 Font = UiHelper.NormalFont,
                 IntegralHeight = false,
                 Size = new Size(360, 140),
@@ -168,6 +170,7 @@ namespace ApplianceManagement.Forms
             lstProduct = new ListBox
             {
                 Visible = false,
+                Location = new Point(-2000, -2000),
                 Font = UiHelper.NormalFont,
                 IntegralHeight = false,
                 Size = new Size(520, 160)
@@ -483,7 +486,20 @@ namespace ApplianceManagement.Forms
                 {
                     selectedProduct = productRepo.GetById(p.ProductID) ?? p;
                     if (selectedProduct != null)
+                    {
                         txtDescription.Text = (selectedProduct.ProductCode ?? "") + " - " + (selectedProduct.ProductName ?? "");
+                        decimal packPrice = selectedProduct.PurchasePrice;
+                        foreach (var line in cart)
+                        {
+                            if (line.ProductID == selectedProduct.ProductID)
+                            {
+                                line.ProductName = selectedProduct.ProductName;
+                                line.PurchasePrice = packPrice;
+                                line.Amount = Math.Round(line.Quantity * packPrice, 2);
+                            }
+                        }
+                        RefreshGrid();
+                    }
                 }
             }
         }
